392ab6ac5f41f24efb5d41fd2147de12cb931dc6
maviszhu <maviszhu1987@gmail.com>
2017-02-06 21:21:13 +0800

    implement current_cart function

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'devise'
 gem 'simple_form'
 gem "carrierwave"
 gem "mini_magick"
+gem "font-awesome-rails"
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,8 @@ class ProductsController < ApplicationController
     end
   end
 
+
+
   def add_to_cart
     @product = Product.find(params[:id])
     current_cart.add_product_to_cart(@product)
